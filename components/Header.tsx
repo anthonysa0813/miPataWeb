8c79dc82010f769cb303b9e6eafd4bff9deac148
anthony <anthonysa0813@gmail.com>
2022-12-14 18:08:53 -0500

    change Hero layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import styles from "../styles/Header.module.css";
 import ButtonPrimary from "./Buttons/ButtonSocial";
 import { GrApple } from "react-icons/gr";
@@ -7,22 +7,44 @@ import { GrApple } from "react-icons/gr";
 // import { AiFillAndroid } from "react-icons/ai";
 
 const Header = () => {
+  const [scrollY, setScrollY] = useState(0);
+
+  const onScroll = useCallback((event: any) => {
+    const { pageYOffset, scrollY } = window;
+    console.log("yOffset", pageYOffset, "scrollY", scrollY);
+    setScrollY(window.pageYOffset);
+  }, []);
+
+  useEffect(() => {
+    //add eventlistener to window
+    window.addEventListener("scroll", onScroll, { passive: true });
+    // remove event on unmount to prevent a memory leak with the cleanup
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);
+
   return (
-    <header className={styles.header}>
+    <header
+      className={`${styles.header} ${scrollY ? styles.backgroundWhite : ""}`}
+    >
       <div className="wrapper">
         <div className={styles.headerContainer}>
           <div className={styles.logoSection}>
-            <h2>miPata</h2>
+            <h2 className={`${scrollY ? styles.textOrange : styles.textWhite}`}>
+              miPata
+            </h2>
+          </div>
+          <div className={styles.menuLinks}>
+            <ul>
+              <li>App mobile</li>
+              <li>About Us</li>
+              <li>Blog</li>
+            </ul>
           </div>
           <div className={styles.socialButtons}>
             <ButtonPrimary>
               <GrApple />
-              {/* <Image
-                src="/applestore.png"
-                alt="Logo de apple store"
-                width={20}
-                height={20}
-              /> */}
               <p
                 className={styles.buttonText}
                 style={{
@@ -51,7 +73,6 @@ const Header = () => {
                   margin: ".25rem",
                 }}
               >
-                {/* <span>Disponible en el </span> */}
                 <span className={styles.text}>Play Store</span>
               </p>
             </ButtonPrimary>
